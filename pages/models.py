7a9596fa40d3c9e98309e960c91bd80b935d1b33
develop3r2104-source <develop3r.2104@gmail.com>
2026-04-25 11:44:27 -0400

    lista de articulos y navegacion base

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
 
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100) # Opcional por ahora
+    content = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import ListView
 from .models import post  # <--- Agrega esta línea
+from .models import Article  # Cambiamos post por Article
 
 class HomePageView(ListView):
     template_name = "home.html"
     model = post  # Ahora Python ya sabe qué es "post"
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "article_list.html"  # Puedes crear un nuevo HTML
+    context_object_name = "articles"     # Nombre personalizado para el HTML
+    ordering = ['-published_date']       # Para que los más nuevos salgan arriba
