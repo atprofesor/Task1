0917ccba41ded28d585136a19126bfa45f59f2b2
develop3r2104-source <develop3r.2104@gmail.com>
2026-05-02 10:42:12 -0400

    login done

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # Agrega esta línea para las URLs de autenticación
 ]
